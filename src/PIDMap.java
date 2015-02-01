d87bd6ccb0f254d93ab4091bc8ef73a49464ec29
Andy Sheu <email>
2015-02-01 05:34:00 -0800

    This code is partially commented and might not compile. This version is different from the PIDTesting in that it was rewritten and has different features.
    Too many things were changed for them to be listed here.
    Ask Andy for more information.

---
+++ src/PIDMap.java
@@ -0,0 +1,22 @@
+package org.usfirst.frc.team668.robot;
+
+public class PIDMap {
+	
+	public static final double TIME_OSCILLATION_MAXIMUM = 1000.0; // placeholder 
+	public static final double CURRENT_MAXIMUM = 100.0; // placeholder
+	
+	public static final double MAXIMUM_TIME = 10.0;
+	
+	public static final double MAXIMUM_PID_ERROR = 100.0;
+	
+	public static final double MAXIMUM_INIT_CURRENT = 80.0;
+	public static final double MAXIMUM_SUSTAIN_CURRENT = 20.0;
+	public static final double CURRENT_CHECK_TIME = 5.0;
+		
+	public static final double PID_ERROR_THRESHOLD = 0.5;
+	
+	public static final int AMOUNT_OF_READINGS = 3; // More sophisticated adjustments are necessary
+	
+	
+	
+}
