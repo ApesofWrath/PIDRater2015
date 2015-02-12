793b1510672223400b36fbe2e41324f0797048a9
Andy <email>
2015-02-11 20:10:46 -0800

    Fixed an extra "'"

--- src/PIDRater.java
+++ src/PIDRater.java
@@ -9,120 +9,137 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  */
 public class PIDRater {
 
-    public static double accuracyScore, timeOscillationScore, currentScore, overallScore;
-    private static double currentTime;
-    private static double currentAccuracy, lastAccuracy, secondToLastAccuracy;
+	public static double accuracyScore, timeOscillationScore, currentScore,
+			overallScore;
+	private static double currentTime;
+	private static double currentAccuracy, lastAccuracy, secondToLastAccuracy;
 
-    private static double timeOscillation, current, accuracy;
-    public static boolean getValueDone;
+	private static double timeOscillation, current, accuracy;
+	public static boolean getValueDone;
 
-    /*
-     The three criteria we will use will be as follows:
-     -Time Oscillation (Integral of Time-Oscillation graph)
-     -Current (Integral of Amps-Secs graph)
-     -Accuracy (At finish, absolute value of distance from percent error 0)
-    
-     Stops:
-     -10 seconds (Safety)
-     -100+ units of absolute value of PID Error
-     -Over 100 amps of current at any given time
-     -Over 20 amps of current after 5 seconds
-     */
-    public static void checkStop() {
-	if (Robot.time.get() >= PIDMap.MAXIMUM_TIME) {
-	    stop("Time");
-	    Robot.disable();
-	}
-	if (Math.abs(Robot.pid.getError()) >= PIDMap.MAXIMUM_PID_ERROR) {
-	    stop("PID Error");
-	    Robot.disable();
-	}
-	if (Robot.hammerTalon.getOutputCurrent() >= PIDMap.MAXIMUM_INIT_CURRENT) {
-	    stop("Current Init");
-	    Robot.disable();
-	}
-	if (Robot.hammerTalon.getOutputCurrent() >= PIDMap.MAXIMUM_SUSTAIN_CURRENT && Robot.time.get() >= PIDMap.CURRENT_CHECK_TIME) {
-	    stop("Current Sustained");
-	    Robot.disable();
-	}
-    }
-
-    public static void stop(String errorMessage) {
-	Robot.hammerTalon.set(0.0);
-	Robot.pid.disable();
-	SmartDashboard.putString("STOP", errorMessage);
-	Robot.noGo(); // Must turn code off then onn
-    }
-
-    public static void getValues() {
-	if (!getValueDone) {
-	    timeOscillation += Robot.time.get() * Math.abs(Robot.pid.getError());
-	    current += Robot.time.get() * Robot.hammerTalon.getOutputCurrent();
-
-	    secondToLastAccuracy = lastAccuracy;
-	    lastAccuracy = currentAccuracy;
-	    currentAccuracy = Robot.pid.getError();
-
-	    // These boolean statements simplify the if statements
-	    boolean a = currentAccuracy < lastAccuracy + PIDMap.PID_ERROR_THRESHOLD;
-	    boolean b = currentAccuracy > lastAccuracy - PIDMap.PID_ERROR_THRESHOLD;
-	    boolean c = currentAccuracy < secondToLastAccuracy + PIDMap.PID_ERROR_THRESHOLD;
-	    boolean d = currentAccuracy > secondToLastAccuracy - PIDMap.PID_ERROR_THRESHOLD;
-
-	    if (a && b) {
-		if (c && d) { // Inside both deadzones -> three consecutive values within 1
-		    getValueDone = true;
-		    accuracy = Math.abs((currentAccuracy + lastAccuracy + secondToLastAccuracy) / 3.0);
-		    // Average of errors shows accuracy. Absolute value is because accuracy isn't negative.
+	/*
+	 * The three criteria we will use will be as follows: -Time Oscillation
+	 * (Integral of Time-Oscillation graph) -Current (Integral of Amps-Secs
+	 * graph) -Accuracy (At finish, absolute value of distance from percent
+	 * error 0)
+	 * 
+	 * Stops: -10 seconds (Safety) -100+ units of absolute value of PID Error
+	 * -Over 100 amps of current at any given time -Over 20 amps of current
+	 * after 5 seconds
+	 */
+	public static void checkStop() {
+		if (Robot.time.get() >= PIDMap.MAXIMUM_TIME) {
+			stop("Time");
+			Robot.disable();
 		}
-	    }
-	} else { // Of it is done, clear values
-	    currentAccuracy = 0;
-	    lastAccuracy = 0;
-	    secondToLastAccuracy = 0;
-	    timeOscillation = 0;
-	    current = 0;
-	    getValueDone = false;
-	    Robot.time.stop();
-	    Robot.time.reset();
-	}
-    }
-
-    public static void processValues() { // Called constantly; Need error handling?
-	accuracyScore = 100 - (10 * accuracy);
-	currentScore = 100 - ((current / PIDMap.CURRENT_MAXIMUM) * 100);
-	timeOscillationScore = 100 - ((timeOscillation / PIDMap.TIME_OSCILLATION_MAXIMUM) * 100);
-
-	if (accuracyScore < 0) {
-	    accuracyScore = 0;
-	    SmartDashboard.putBoolean("Accuracy Score", false);
-	} else {
-	    SmartDashboard.putBoolean("Accuracy Score", true);
-	}
-	if (currentScore < 0) {
-	    currentScore = 0;
-	    SmartDashboard.putBoolean("Current Score", false);
-	} else {
-	    SmartDashboard.putBoolean("Current Score", true);
-	}
-	if (timeOscillationScore < 0) {
-	    timeOscillationScore = 0;
-	    SmartDashboard.putBoolean("Time Oscillation Score", false);
-	} else {
-	    SmartDashboard.putBoolean("Time Oscillation Score", true);
+		if (Math.abs(Robot.pid.getError()) >= PIDMap.MAXIMUM_PID_ERROR) {
+			stop("PID Error");
+			Robot.disable();
+		}
+		if (Robot.hammerTalon.getOutputCurrent() >= PIDMap.MAXIMUM_INIT_CURRENT) {
+			stop("Current Init");
+			Robot.disable();
+		}
+		if (Robot.hammerTalon.getOutputCurrent() >= PIDMap.MAXIMUM_SUSTAIN_CURRENT
+				&& Robot.time.get() >= PIDMap.CURRENT_CHECK_TIME) {
+			stop("Current Sustained");
+			Robot.disable();
+		}
 	}
 
-	// Accuracy is rated from 0-100
-	// Current is scaled into a percentage relative to the maximum and them rated from 0-100
-	// Time Oscillation is also scaled to max and rated 0-100
-    }
+	public static void stop(String errorMessage) {
+		Robot.hammerTalon.set(0.0);
+		Robot.pid.disable();
+		SmartDashboard.putString("STOP", errorMessage);
+		Robot.noGo(); // Must turn code off then onn
+	}
 
-    public static double ratePID() {
-	overallScore = (accuracyScore + currentScore + timeOscillationScore) / 3.0; // Average of three scores -> percentage scale
-	SmartDashboard.putNumber("Accuracy Score", accuracyScore);
-	SmartDashboard.putNumber("Current Score", currentScore);
-	SmartDashboard.putNumber("Time Oscillation Score", timeOscillationScore);
-	SmartDashboard.putNumber("Overall Score", overallScore);
-	return overallScore; // Overall score is out of 100
-    }
+	public static void getValues() {
+		if (!getValueDone) {
+			timeOscillation += Robot.time.get()
+					* Math.abs(Robot.pid.getError());
+			current += Robot.time.get() * Robot.hammerTalon.getOutputCurrent();
+
+			secondToLastAccuracy = lastAccuracy;
+			lastAccuracy = currentAccuracy;
+			currentAccuracy = Robot.pid.getError();
+
+			// These boolean statements simplify the if statements
+			boolean a = currentAccuracy < lastAccuracy
+					+ PIDMap.PID_ERROR_THRESHOLD;
+			boolean b = currentAccuracy > lastAccuracy
+					- PIDMap.PID_ERROR_THRESHOLD;
+			boolean c = currentAccuracy < secondToLastAccuracy
+					+ PIDMap.PID_ERROR_THRESHOLD;
+			boolean d = currentAccuracy > secondToLastAccuracy
+					- PIDMap.PID_ERROR_THRESHOLD;
+
+			if (a && b) {
+				if (c && d) { // Inside both deadzones -> three consecutive
+								// values within 1
+					getValueDone = true;
+					accuracy = Math
+							.abs((currentAccuracy + lastAccuracy + secondToLastAccuracy) / 3.0);
+					// Average of errors shows accuracy. Absolute value is
+					// because accuracy isn't negative.
+				}
+			}
+		} else { // Of it is done, clear values
+			currentAccuracy = 0;
+			lastAccuracy = 0;
+			secondToLastAccuracy = 0;
+			timeOscillation = 0;
+			current = 0;
+			getValueDone = false;
+			Robot.time.stop();
+			Robot.time.reset();
+		}
+	}
+
+	public static void processValues() { // Called constantly; Need error
+											// handling?
+		accuracyScore = 100 - (10 * accuracy);
+		currentScore = 100 - ((current / PIDMap.CURRENT_MAXIMUM) * 100);
+		timeOscillationScore = 100 - ((timeOscillation / PIDMap.TIME_OSCILLATION_MAXIMUM) * 100);
+
+		if (accuracyScore < 0) {
+			accuracyScore = 0;
+			SmartDashboard.putBoolean("Accuracy Score", false);
+		} else {
+			SmartDashboard.putBoolean("Accuracy Score", true);
+		}
+		if (currentScore < 0) {
+			currentScore = 0;
+			SmartDashboard.putBoolean("Current Score", false);
+		} else {
+			SmartDashboard.putBoolean("Current Score", true);
+		}
+		if (timeOscillationScore < 0) {
+			timeOscillationScore = 0;
+			SmartDashboard.putBoolean("Time Oscillation Score", false);
+		} else {
+			SmartDashboard.putBoolean("Time Oscillation Score", true);
+		}
+
+		// Accuracy is rated from 0-100
+		// Current is scaled into a percentage relative to the maximum and them
+		// rated from 0-100
+		// Time Oscillation is also scaled to max and rated 0-100
+	}
+
+	public static double ratePID() {
+		overallScore = (accuracyScore + currentScore + timeOscillationScore) / 3.0; // Average
+																					// of
+																					// three
+																					// scores
+																					// ->
+																					// percentage
+																					// scale
+		SmartDashboard.putNumber("Accuracy Score", accuracyScore);
+		SmartDashboard.putNumber("Current Score", currentScore);
+		SmartDashboard
+				.putNumber("Time Oscillation Score", timeOscillationScore);
+		SmartDashboard.putNumber("Overall Score", overallScore);
+		return overallScore; // Overall score is out of 100
+	}
 }
--- src/Robot.java
+++ src/Robot.java
@@ -21,258 +21,264 @@ import edu.wpi.first.wpilibj.Timer;
  */
 public class Robot extends IterativeRobot {
 
-    /**
-     * This function is run when the robot is first started up and should be
-     * used for any initialization code.
-     */
-    public static CANTalon hammerTalon;
-    public static PowerDistributionPanel pdp;
-    public static Encoder hammerEncoder;
-    public static PIDController pid;
-    public static Joystick joystickOp;
-    public static Timer time, ti;
-    public static double pVal = -0.018; // These are the current tuning values
-    public static double iVal = -0.001; // They can still be changed in the code
-    public static double dVal = -0.030;
-    public static boolean go = true;
-    public static double scale = 0.001;
-    public static int setpoint = 65;
-    public static double current = 0.0;
-    public static boolean button3 = false, button4 = false, button5 = false, button6 = false,
-	    button7 = false, button8 = false, button9 = false,
-	    button10 = false, button11 = false, button12 = false;
-    public static double[][] pidTesting, pidValues;
-    public static int i;
-
-    public void robotInit() {
-	hammerTalon = new CANTalon(2);
-	hammerEncoder = new Encoder(9, 8);
-	hammerEncoder.setPIDSourceParameter(PIDSourceParameter.kDistance);
-	hammerTalon.reverseOutput(false);
-	pid = new PIDController(0.0, 0.0, 0.0, hammerEncoder, hammerTalon);
-	joystickOp = new Joystick(1);
-	hammerEncoder.reset();
-	pdp = new PowerDistributionPanel();
-	time = new Timer();
-    }
-
-    public void autonomousInit() {
-	teleopInit();
-    }
-
-    public void disabledInit() {
-	super.disabledInit(); // Calls the Iterative Robot disabledInit() because there is no point in us writing one
-    }
-
-    /**
-     * This function is called periodically during autonomous
-     */
-    public void autonomousPeriodic() {
-	teleopPeriodic();
-    }
-
-    public void teleopInit() {
-	hammerEncoder.reset();
-	pid.setPID(pVal, iVal, dVal);
-	pid.setSetpoint(setpoint); // Predetermined setpoint
-	go = true;
-    }
-
-    /**
-     * This function is called periodically during operator control
-     */
-    public void teleopPeriodic() {
-	if (!pid.isEnable()) { // disable() will automatically enable PID after 8 seconds
-	    disable();
-	} else if (go == false) {
-	    Timer.delay(Long.MAX_VALUE); // Purposeful thread hanging
-	} else {
-	    checkFlip();
-	    printDashboard();
-
-	    pid.setPID(pVal, iVal, dVal);
-	    pid.setSetpoint(setpoint);
-	}
-    }
-
-    /**
-     * This function is called periodically during test mode
-     */
-    public void testInit() {
-	pidTesting = new double[50][7]; // 50 is a random value
-	// Array > ArrayList because it is so much simpler
-
-	/*
-	 pidTesting values:
-	 0   1	2   3		    4			    5		    6
-	 P   I	D   AccuracyScore   TimeOscillationScore    CurrentScore    OverallScore    
+	/**
+	 * This function is run when the robot is first started up and should be
+	 * used for any initialization code.
 	 */
-	pidValues = new double[50][3];
-	for (i = 0; i < 50; i++) {
-	    pidValues[i][0] = -Math.random();
-	    pidValues[i][1] = -Math.random();
-	    pidValues[i][2] = -Math.random();
+	public static CANTalon hammerTalon;
+	public static PowerDistributionPanel pdp;
+	public static Encoder hammerEncoder;
+	public static PIDController pid;
+	public static Joystick joystickOp;
+	public static Timer time, ti;
+	public static double pVal = -0.018; // These are the current tuning values
+	public static double iVal = -0.001; // They can still be changed in the code
+	public static double dVal = -0.030;
+	public static boolean go = true;
+	public static double scale = 0.001;
+	public static int setpoint = 65;
+	public static double current = 0.0;
+	public static boolean button3 = false, button4 = false, button5 = false,
+			button6 = false, button7 = false, button8 = false, button9 = false,
+			button10 = false, button11 = false, button12 = false;
+	public static double[][] pidTesting, pidValues;
+	public static int i;
+
+	public void robotInit() {
+		hammerTalon = new CANTalon(2);
+		hammerEncoder = new Encoder(9, 8);
+		hammerEncoder.setPIDSourceParameter(PIDSourceParameter.kDistance);
+		hammerTalon.reverseOutput(false);
+		pid = new PIDController(0.0, 0.0, 0.0, hammerEncoder, hammerTalon);
+		joystickOp = new Joystick(1);
+		hammerEncoder.reset();
+		pdp = new PowerDistributionPanel();
+		time = new Timer();
 	}
 
-	i = 0;
-    }
-
-    public void testPeriodic() {
-	if (!PIDRater.getValueDone) {
-	    pVal = pidValues[i][0];
-	    iVal = pidValues[i][1];
-	    dVal = pidValues[i][2];
-	    teleopPeriodic();
-	    ratePID();
-	} else {
-	    pidTesting[i][0] = pid.getP();
-	    pidTesting[i][1] = pid.getI();
-	    pidTesting[i][2] = pid.getD();
-	    pidTesting[i][3] = PIDRater.accuracyScore;
-	    pidTesting[i][4] = PIDRater.timeOscillationScore;
-	    pidTesting[i][5] = PIDRater.currentScore;
-	    pidTesting[i][6] = PIDRater.overallScore;
-
-	    pid.setPID(pVal, iVal, dVal);
-	    teleopInit();
-	    PIDRater.getValueDone = false;
-	    i++;
-	}
-    }
-
-    public void checkFlip() {
-	if (hammerEncoder.getDistance() > 150.0) {
-	    pid.disable();
-	    disable();
-	    go = false;
-	}
-    }
-
-    public void pidPeriodic() {
-	pid.setSetpoint(setpoint);
-
-	checkFlip(); // Redundancy in safety is good
-	printDashboard();
-	doButtons();
-	checkFlip(); // Redundancy in safety is good
-
-	pid.setPID(pVal, iVal, dVal);
-	if (joystickOp.getRawButton(2)) {
-	    pid.disable(); // THIS DISABLE IS FOR THE PID, NOT THE ROBOT
-	    SmartDashboard.putBoolean("Disabled", true);
-	} else if (joystickOp.getRawButton(1)) { // We want disable to be priority over enable and only have one work
-	    pid.enable();
-	    SmartDashboard.putBoolean("Disabled", false);
-	}
-    }
-
-    public static void doButtons() {
-	if (joystickOp.getRawButton(5) && button5 == false) { // Scale
-	    scale *= 10;
-	    button5 = true;
-	} else if (!joystickOp.getRawButton(5)) {
-	    button5 = false;
-	}
-	if (joystickOp.getRawButton(3) && button3 == false) {
-	    scale /= 10;
-	    button3 = true;
-	} else if (!joystickOp.getRawButton(3)) {
-	    button3 = false;
+	public void autonomousInit() {
+		teleopInit();
 	}
 
-	if (joystickOp.getRawButton(6) && button6 == false) { // Setpoints are integers (not doubles)
-	    setpoint += scale * 1000;
-	    button6 = true;
-	} else if (!joystickOp.getRawButton(6)) {
-	    button6 = false;
-	}
-	if (joystickOp.getRawButton(4) && button4 == false) {
-	    setpoint -= scale * 1000;
-	    button4 = true;
-	} else if (!joystickOp.getRawButton(4)) {
-	    button4 = false;
+	public void disabledInit() {
+		super.disabledInit(); // Calls the Iterative Robot disabledInit()
+								// because there is no point in us writing one
 	}
 
-	if (joystickOp.getRawButton(7) && button7 == false) {
-	    pVal += scale;
-	    button7 = true;
-	} else if (!joystickOp.getRawButton(7)) {
-	    button7 = false;
+	/**
+	 * This function is called periodically during autonomous
+	 */
+	public void autonomousPeriodic() {
+		teleopPeriodic();
 	}
-	if (joystickOp.getRawButton(8) && button8 == false) {
-	    pVal -= scale;
-	    button8 = true;
-	} else if (!joystickOp.getRawButton(8)) {
-	    button8 = false;
-	}
-	if (joystickOp.getRawButton(9) && button9 == false) {
-	    iVal += scale;
-	    button9 = true;
-	} else if (!joystickOp.getRawButton(9)) {
-	    button9 = false;
-	}
-	if (joystickOp.getRawButton(10) && button10 == false) {
-	    iVal -= scale;
-	    button10 = true;
-	} else if (!joystickOp.getRawButton(10)) {
-	    button10 = false;
-	}
-	if (joystickOp.getRawButton(11) && button11 == false) {
-	    dVal += scale;
-	    button11 = true;
-	} else if (!joystickOp.getRawButton(11)) {
-	    button11 = false;
-	}
-	if (joystickOp.getRawButton(12) && button12 == false) {
-	    dVal -= scale;
-	    button12 = true;
-	} else if (!joystickOp.getRawButton(12)) {
-	    button12 = false;
-	}
-    }
 
-    public static void printDashboard() {
-	SmartDashboard.putNumber("P", pid.getP());
-	SmartDashboard.putNumber("I", pid.getI());
-	SmartDashboard.putNumber("D", pid.getD());
-	SmartDashboard.putNumber("PID Error", pid.getError());
-	SmartDashboard.putNumber("Scale", scale);
-	SmartDashboard.putNumber("Setpoint", setpoint);
-	SmartDashboard.putBoolean("Disabled", pid.isEnabled());
-
-	SmartDashboard.putNumber("Hammer Encoder Distance", hammerEncoder.getDistance());
-	SmartDashboard.putNumber("Current", hammerTalon.getOutputCurrent());
-	SmartDashboard.putNumber("Motor Speed", hammerTalon.getSpeed());
-    }
-
-    public static void enable() {
-	pid.enable();
-	SmartDashboard.putBoolean("Disabled", false);
-    }
-
-    public static void disable() {
-	pid.disable();
-	SmartDashboard.putBoolean("Disabled", true);
-	ti = new Timer();
-	ti.reset();
-	ti.start();
-	if (ti.get() >= 8) {
-	    enable();
-	} else {
-	    disable();
+	public void teleopInit() {
+		hammerEncoder.reset();
+		pid.setPID(pVal, iVal, dVal);
+		pid.setSetpoint(setpoint); // Predetermined setpoint
+		go = true;
 	}
-    }
 
-    public static void noGo() {
-	go = false;
-	Timer.delay(Double.MAX_VALUE); // This will purposefully hang the thread
-    }
+	/**
+	 * This function is called periodically during operator control
+	 */
+	public void teleopPeriodic() {
+		if (!pid.isEnable()) { // disable() will automatically enable PID after
+								// 8 seconds
+			disable();
+		} else if (go == false) {
+			Timer.delay(Long.MAX_VALUE); // Purposeful thread hanging
+		} else {
+			checkFlip();
+			printDashboard();
 
-    public static void ratePID() { // All PID algorithms called here
-	time.start();
-	PIDRater.checkStop();
-	PIDRater.getValues();
-	PIDRater.processValues();
-	PIDRater.ratePID();
-    }
+			pid.setPID(pVal, iVal, dVal);
+			pid.setSetpoint(setpoint);
+		}
+	}
+
+	/**
+	 * This function is called periodically during test mode
+	 */
+	public void testInit() {
+		pidTesting = new double[50][7]; // 50 is a random value
+		// Array > ArrayList because it is so much simpler
+
+		/*
+		 * pidTesting values: 0 1 2 3 4 5 6 P I D AccuracyScore
+		 * TimeOscillationScore CurrentScore OverallScore
+		 */
+		pidValues = new double[50][3];
+		for (i = 0; i < 50; i++) {
+			pidValues[i][0] = -Math.random();
+			pidValues[i][1] = -Math.random();
+			pidValues[i][2] = -Math.random();
+		}
+
+		i = 0;
+	}
+
+	public void testPeriodic() {
+		if (!PIDRater.getValueDone) {
+			pVal = pidValues[i][0];
+			iVal = pidValues[i][1];
+			dVal = pidValues[i][2];
+			teleopPeriodic();
+			ratePID();
+		} else {
+			pidTesting[i][0] = pid.getP();
+			pidTesting[i][1] = pid.getI();
+			pidTesting[i][2] = pid.getD();
+			pidTesting[i][3] = PIDRater.accuracyScore;
+			pidTesting[i][4] = PIDRater.timeOscillationScore;
+			pidTesting[i][5] = PIDRater.currentScore;
+			pidTesting[i][6] = PIDRater.overallScore;
+
+			pid.setPID(pVal, iVal, dVal);
+			teleopInit();
+			PIDRater.getValueDone = false;
+			i++;
+		}
+	}
+
+	public void checkFlip() {
+		if (hammerEncoder.getDistance() > 150.0) {
+			pid.disable();
+			disable();
+			go = false;
+		}
+	}
+
+	public void pidPeriodic() {
+		pid.setSetpoint(setpoint);
+
+		checkFlip(); // Redundancy in safety is good
+		printDashboard();
+		doButtons();
+		checkFlip(); // Redundancy in safety is good
+
+		pid.setPID(pVal, iVal, dVal);
+		if (joystickOp.getRawButton(2)) {
+			pid.disable(); // THIS DISABLE IS FOR THE PID, NOT THE ROBOT
+			SmartDashboard.putBoolean("Disabled", true);
+		} else if (joystickOp.getRawButton(1)) { // We want disable to be
+													// priority over enable and
+													// only have one work
+			pid.enable();
+			SmartDashboard.putBoolean("Disabled", false);
+		}
+	}
+
+	public static void doButtons() {
+		if (joystickOp.getRawButton(5) && button5 == false) { // Scale
+			scale *= 10;
+			button5 = true;
+		} else if (!joystickOp.getRawButton(5)) {
+			button5 = false;
+		}
+		if (joystickOp.getRawButton(3) && button3 == false) {
+			scale /= 10;
+			button3 = true;
+		} else if (!joystickOp.getRawButton(3)) {
+			button3 = false;
+		}
+
+		if (joystickOp.getRawButton(6) && button6 == false) { // Setpoints are
+																// integers (not
+																// doubles)
+			setpoint += scale * 1000;
+			button6 = true;
+		} else if (!joystickOp.getRawButton(6)) {
+			button6 = false;
+		}
+		if (joystickOp.getRawButton(4) && button4 == false) {
+			setpoint -= scale * 1000;
+			button4 = true;
+		} else if (!joystickOp.getRawButton(4)) {
+			button4 = false;
+		}
+
+		if (joystickOp.getRawButton(7) && button7 == false) {
+			pVal += scale;
+			button7 = true;
+		} else if (!joystickOp.getRawButton(7)) {
+			button7 = false;
+		}
+		if (joystickOp.getRawButton(8) && button8 == false) {
+			pVal -= scale;
+			button8 = true;
+		} else if (!joystickOp.getRawButton(8)) {
+			button8 = false;
+		}
+		if (joystickOp.getRawButton(9) && button9 == false) {
+			iVal += scale;
+			button9 = true;
+		} else if (!joystickOp.getRawButton(9)) {
+			button9 = false;
+		}
+		if (joystickOp.getRawButton(10) && button10 == false) {
+			iVal -= scale;
+			button10 = true;
+		} else if (!joystickOp.getRawButton(10)) {
+			button10 = false;
+		}
+		if (joystickOp.getRawButton(11) && button11 == false) {
+			dVal += scale;
+			button11 = true;
+		} else if (!joystickOp.getRawButton(11)) {
+			button11 = false;
+		}
+		if (joystickOp.getRawButton(12) && button12 == false) {
+			dVal -= scale;
+			button12 = true;
+		} else if (!joystickOp.getRawButton(12)) {
+			button12 = false;
+		}
+	}
+
+	public static void printDashboard() {
+		SmartDashboard.putNumber("P", pid.getP());
+		SmartDashboard.putNumber("I", pid.getI());
+		SmartDashboard.putNumber("D", pid.getD());
+		SmartDashboard.putNumber("PID Error", pid.getError());
+		SmartDashboard.putNumber("Scale", scale);
+		SmartDashboard.putNumber("Setpoint", setpoint);
+		SmartDashboard.putBoolean("Disabled", pid.isEnabled());
+
+		SmartDashboard.putNumber("Hammer Encoder Distance",
+				hammerEncoder.getDistance());
+		SmartDashboard.putNumber("Current", hammerTalon.getOutputCurrent());
+		SmartDashboard.putNumber("Motor Speed", hammerTalon.getSpeed());
+	}
+
+	public static void enable() {
+		pid.enable();
+		SmartDashboard.putBoolean("Disabled", false);
+	}
+
+	public static void disable() {
+		pid.disable();
+		SmartDashboard.putBoolean("Disabled", true);
+		ti = new Timer();
+		ti.reset();
+		ti.start();
+		if (ti.get() >= 8) {
+			enable();
+		} else {
+			disable();
+		}
+	}
+
+	public static void noGo() {
+		go = false;
+		Timer.delay(Double.MAX_VALUE); // This will purposefully hang the thread
+	}
+
+	public static void ratePID() { // All PID algorithms called here
+		time.start();
+		PIDRater.checkStop();
+		PIDRater.getValues();
+		PIDRater.processValues();
+		PIDRater.ratePID();
+	}
 }
